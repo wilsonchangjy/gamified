4debdf1ff0a1046f655316126fd4de8094b93a9c
Wilson Chang <88810306+wilsonchangjy@users.noreply.github.com>
2023-05-04 05:14:26 +0800

    Delete src directory

--- src/menu.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function(){
-    var scroll = document.querySelector('.back-top');
-    scroll.classList.toggle("active", window.scrollY > 650);
-
-    if (window.scrollY > 325) {
-        $('.menu-button').toggleClass("click", false);
-        $('.side-bar').toggleClass("show", false);
-    }
-});
